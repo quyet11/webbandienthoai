9918ddd5d1145cb585df0d8dd24feb2466f0fa2f
LAPTOP-EKVM5V04\admin <quyetprime1234@gmail.com>
2023-08-28 02:29:55 +0700

    upload sourrce code

---
+++ data/products.js
@@ -0,0 +1,438 @@
+var list_products = [{
+    "name": "iPhone 14 Pro Max 128GB",
+    "company": "Apple",
+    "img": "img/products/14promax.webp",
+    "price": "29.390.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "26.390.000"
+    },
+    "detail": {
+        "screen": "Super Retina XDR OLED",
+        "os": "iOS 16",
+        "camara": "Camera chính: 48 MP, f/1.8, 24mm, 1.22µm, PDAF, OIS,Camera góc siêu rộng: 12 MP, f/2.2, 13mm, 120˚, 1.4µm, PDAF Camera tele: 12 MP, f/2.8, 77mm (telephoto), PDAF, OIS, 3x optical zoom Cảm biến độ sâu TOF 3D LiDAR",
+        "camaraFront": "Camera selfie: 12 MP, f/1.9, 23mm, 1/3.6",
+        "cpu": "Apple A16 Bionic 6-core",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "Không",
+        "battery": "3223 mAh"
+    },
+    "masp": "App2"
+}, {
+    "name": "SamSung Galaxy Z Fold5",
+    "company": "Samsung",
+    "img": "https://cdn2.cellphones.com.vn/300x,webp,q100/media/catalog/product/g/a/galaxy-z-fold-5-xanh-1.jpg",
+    "price": "40.990.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Dynamic AMOLED 2X",
+        "os": "Android 13",
+        "camara": "Camera siêu rộng: 12MP F2.2, 123°, 1.12μm Camera góc rộng: 50MP, F1.8, Dual Pixel AF, OIS, 2.0μm Camera Tele: 10 MP, F2.4, PDAF, OIS, 1.0μm, zoom 3X, zoom kỹ thuật số 30X",
+        "camaraFront": "Camera bên ngoài:10 MP, f/2.2 Camera bên trong: 4 MP, F1.8",
+        "cpu": "Snapdragon 8 Gen 2 for Galaxy (4nm) 8 nhân",
+        "ram": "12 GB",
+        "rom": "512 GB",
+        "microUSB": "Không có",
+        "battery": "4400 mAh"
+    },
+    "masp": "Sam0"
+}, {
+    "name": "OPPO Find N2 Flip",
+    "company": "Oppo",
+    "img": "https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/n/2/n2_flip_-_combo_product_-_black.png",
+    "price": "19.990.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "AMOLED",
+        "os": "Android 13",
+        "camara": "Camera góc rộng: 50MP, f/1.8, 23mm, PDAF Camera góc siêu rộng 8MP, f/2.2, 112˚",
+        "camaraFront": "Camera góc rộng: 32MP, f/2.4, 22mm, AF",
+        "cpu": "MediaTek 9000+ (4nm)",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "4300 mAh"
+    },
+    "masp": "Opp0"
+}, {
+    "name": "Samsung Galaxy S23 Ultra 256GB",
+    "company": "Samsung",
+    "img": "img/products/23ultra.webp",
+    "price": "23.790.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+    "detail": {
+        "screen": "Dynamic AMOLED 2X",
+        "os": "Android",
+        "camara": "Siêu rộng: 12MP F2.2 (Dual Pixel AF),Chính: 200MP F1.7 OIS ±3° (Super Quad Pixel AF),Tele 1: 10MP F4.9 (10X, Dual Pixel AF) OIS,Tele 2: 10MP F2.4 (3X, Dual Pixel AF) OIS Thu phóng chuẩn không gian 100X",
+        "camaraFront": "12MP F2.2 (Dual Pixel AF)",
+        "cpu": "Snapdragon 8 Gen 2 (4 nm)",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "5.000mAh"
+    },
+    "masp": "Sam1"
+},  {
+    "name": "SamSung Galaxy Z Flip5",
+    "company": "Samsung",
+    "img": "https://cdn2.cellphones.com.vn/300x,webp,q100/media/catalog/product/d/4/d4a672c5-4709-4056-9f7f-72d6d70c2c1d.jpg",
+    "price": "25.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "Dynamic AMOLED 2X",
+        "os": "Android 13",
+        "camara": "Camera siêu rộng: 12MP, F2.2, 123°, 1.12 μm, FF Camera chính: 12MP, F1.8, Dual Pixel, 1.8μm, OIS",
+        "camaraFront": "10MP, F2.4, 1.22μm",
+        "cpu": "Snapdragon 8 Gen 2 for Galaxy",
+        "ram": "8 GB",
+        "rom": "512 GB",
+        "microUSB": "Không",
+        "battery": "3700 mAh"
+    },
+    "masp": "Sam2"
+},  {
+    "name": "Mobiistar X",
+    "company": "Mobiistar",
+    "img": "img/products/mobiistar-x-3-600x600.jpg",
+    "price": "1.990.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 5.86', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "16 MP và 5 MP (2 camera)",
+        "camaraFront": "16 MP",
+        "cpu": "MediaTek MT6762 8 nhân 64-bit (Helio P22)",
+        "ram": "4 GB",
+        "rom": "32 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3000 mAh"
+    },
+    "masp": "Mob0"
+},    {
+    "name": "Xiaomi 13 8GB 256GB",
+    "company": "Xiaomi",
+    "img": "https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/p/m/pms_1670745783.80967984.png",
+    "price": "18.490.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "AMOLED",
+        "os": "MIUI 14, Android 13",
+        "camara": "Camera chính: 50MP, f/1.8, HyperOIS Camera tele của Leica: 10MP, f/2.0, OIS Camera góc siêu rộng Leica: 12MP, f/2.2,",
+        "camaraFront": "Camera selfie trong màn hình 32MP, f/2.0",
+        "cpu": "Chip Snapdragon® 8 thế hệ thứ 2",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "4500 mAh"
+    },
+    "masp": "Xia3"
+}, {
+    "name": "Huawei P30 Lite",
+    "company": "Huawei",
+    "img": "https://cdn2.cellphones.com.vn/x/media/catalog/product/1/2/1232-500x500_2.png",
+    "price": "7.490.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD",
+        "os": "Android v9.0 (Pie)",
+        "camara": "24MP + 8MP + 2MP",
+        "camaraFront": "32MP",
+        "cpu": "Kirin 710",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "Có, sử dụng chung khe SIM 2",
+        "battery": "4300 mAh, có sạc nhanh"
+    },
+    "masp": "Hua0"
+}, {
+    "name": "Nokia black future",
+    "company": "Nokia",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/22701/dien-thoai-di-dong-Nokia-1280-dienmay.com-l.jpg",
+    "price": "999.9999.999.999",
+    "star": 5,
+    "rateCount": 9999,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+    "detail": {
+        "screen": "4K, Chống nước, Chống trầy",
+        "os": "iOS + Android song song",
+        "camara": "Bộ tứ camara tàng hình",
+        "camaraFront": "Chuẩn thế giới 50MP",
+        "cpu": "16 nhân 128 bit",
+        "ram": "Không giới hạn",
+        "rom": "Dùng thoải mái",
+        "microUSB": "Không cần",
+        "battery": "Không cần sạc"
+    },
+    "masp": "Nok1"
+},   {
+    "name": "realme 11 Pro 8GB 256GB",
+    "company": "Realme",
+    "img": "https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/r/e/realme-11-pro.png",
+    "price": "11.690.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "AMOLED",
+        "os": "Android 13",
+        "camara": "Camera góc rộng: 100 MP, f/1.75, 26mm, PDAF, OIS Camera chiều sâu: 2 MP, f/2.4, 22mm",
+        "camaraFront": "Camera góc rộng: 16 MP, f/2.5, 25mm",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "5000 mAh"
+    },
+    "masp": "Rea2"
+},  {
+    "name": "Philips E103",
+    "company": "Philips",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/73727/philips-e103-9-300x300.jpg",
+    "price": "260.000",
+    "star": 5,
+    "rateCount": 126,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "TFT, 1.77', 65.536 màu",
+        "os": "Không",
+        "camara": "Không",
+        "camaraFront": "Không",
+        "cpu": "Không",
+        "ram": "Không",
+        "rom": "Không",
+        "microUSB": "Không",
+        "battery": "1050 mAh"
+    },
+    "masp": "Phi2"
+},  {
+    "name": "Vivo V27e 8GB 256GB ",
+    "company": "Vivo",
+    "img": "https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/t/_/t_i_xu_ng_23__4_6.png",
+    "price": "8.990.000",
+    "star": 4,
+    "rateCount": 60,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "AMOLED",
+        "os": "Funtouch OS 13",
+        "camara": "Camera góc rộng: 64MP, f/1.8, PDAF, OIS Camera macro: 2MP, f/2.4 Camera đo độ sâu: 2MP, f/2.4",
+        "camaraFront": "Trước 32MP",
+        "cpu": "Mediatek MT8781 Helio G99 (6nm)",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "microUSB": "microSDXC",
+        "battery": "4600 mAh"
+    },
+    "masp": "Viv3"
+}, {
+    "name": "Mobell M789",
+    "company": "Mobell",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/92016/mobell-m789-8-300x300.jpg",
+    "price": "550.000",
+    "star": 4,
+    "rateCount": 37,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "TFT, 2.4', 65.536 màu",
+        "os": "Không",
+        "camara": "Không",
+        "camaraFront": "Không",
+        "cpu": "Không",
+        "ram": "Không",
+        "rom": "Không",
+        "microUSB": "Không",
+        "battery": "1200 mAh"
+    },
+    "masp": "Mobe0"
+}, {
+    "name": "Itel it2161",
+    "company": "Itel",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/193989/itel-it2161-600x600.jpg",
+    "price": "170.000",
+    "star": 5,
+    "rateCount": 33,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "TFT, 1.77', WVGA",
+        "os": "Không",
+        "camara": "Không",
+        "camaraFront": "Không",
+        "cpu": "Không",
+        "ram": "Không",
+        "rom": "Không",
+        "microUSB": "MicroSD, hỗ trợ tối đa 32 GB",
+        "battery": "1000 mAh"
+    },
+    "masp": "Ite3"
+},  {
+    "name": "Coolpad Tab Tasker 10 Wifi",
+    "company": "Coolpad",
+    "img": "https://cdn2.cellphones.com.vn/x/media/catalog/product/c/_/c_c_xa.jpg",
+    "price": "1.190.000",
+    "star": 1,
+    "rateCount": 1,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 5', WVGA",
+        "os": "Android 10",
+        "camara": "5 MP",
+        "camaraFront": "2 MP",
+        "cpu": "Snapdragon 215",
+        "ram": "3 GB",
+        "rom": "32 GB",
+        "microUSB": "MicroSD",
+        "battery": "6000 mAh"
+    },
+    "masp": "Coo2"
+},  {
+    "name": "iPhone 14 Pro Max 512GB",
+    "company": "Apple",
+    "img": "https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/t/_/t_m-iphone-14-pro_2.png",
+    "price": "35.290.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "Super Retina XDR OLED",
+        "os": "iOS 16",
+        "camara": "Camera chính: 48 MP, f/1.8, 24mm, OIS Camera góc siêu rộng: 12 MP, f/2.2, 13mm, 120˚ Camera tele: 12 MP, f/2.8, 77mm, OIS, 3x optical zoom Cảm biến độ sâu TOF 3D LiDAR",
+        "camaraFront": "Camera selfie: 12 MP, f/1.9, 23mm, PDAF",
+        "cpu": "Apple A16 Bionic 6-core",
+        "ram": "6 GB",
+        "rom": "512 GB",
+        "microUSB": "Không",
+        "battery": "4323 mAh"
+    },
+    "masp": "App5"
+}, {
+    "name": "iPad 2018 Wifi 32GB",
+    "company": "Apple",
+    "img": "img/products/ipad-wifi-32gb-2018-thumb-600x600.jpg",
+    "price": "3.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "LED-backlit LCD, 9.7p''",
+        "os": "\tiOS 11.3",
+        "camara": "8 MP",
+        "camaraFront": "1.2 MP",
+        "cpu": "Apple A10 Fusion, 2.34 GHz",
+        "ram": "2 GB",
+        "rom": "32 GB",
+        "microUSB": "Không",
+        "battery": "Chưa có thông số cụ thể"
+    },
+    "masp": "App1"
+},{
+    "name": "iPhone 11 64GB",
+    "company": "Apple",
+    "img": "img/products/1164.webp",
+    "price": "11.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "10.550.000"
+    },
+    "detail": {
+        "screen": "IPS LCD",
+        "os": "iOS 13 hoặc cao hơn (Tùy vào phiên bản phát hành)",
+        "camara": "Camera kép 12MP:- Camera góc rộng: ƒ/1.8 aperture- Camera siêu rộng: ƒ/2.4 aperture",
+        "camaraFront": "12 MP, ƒ/2.2 aperture",
+        "cpu": "A13 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "Không",
+        "battery": "3110 mAh"
+    },
+    "masp": "App3"
+}, {
+    "name": "Huawei P30",
+    "company": "Huawei",
+    "img": "https://cdn2.cellphones.com.vn/x/media/catalog/product/e/l/elle_aurora_rear_rgb-500x500-_2__1_1.png",
+    "price": "18.990.000",
+    "star": 4,
+    "rateCount": 804,
+    "promo": {
+        "name": "giareonline",
+        "value": "17.990.000"
+    },
+    "detail": {
+        "screen": "OLED, cảm ứng điện dung 16.7 triệu màu",
+        "os": "Android v9.0 (Pie)",
+        "camara": "40 MP, 16 MP và 8 MP (3 camera) ",
+        "camaraFront": "32 MP",
+        "cpu": "HiSilicon Kirin 980",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "microUSB": "Không",
+        "battery": "3650 mAh"
+    },
+    "masp": "Hua3"
+}]
